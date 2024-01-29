9045c07bd8716a6d7d75a62e110b886f70d5e6f4
mahesh2121 <maheshgaikwad128@gmail.com>
2024-01-29 20:51:34 +0530

    tflambda fix syntax error and taint lambda function

--- tflambda/main.tf
+++ tflambda/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_iam_role" "lambda_role" {
-    name = "terraform_aws_lambda_role"
-    assume_role_policy = <<EOF
+  name               = "terraform_aws_lambda_role"
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -19,15 +19,15 @@ resource "aws_iam_role" "lambda_role" {
     ]
 }
 EOF
-
+}
 
 # Iam policy for logging from a lambda
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
-    name = "aws_iam_policy_for_terraform_aws_lambda_role"
-    path = "/"
-    description = "AWS IAM policy for managing aws lambda role"
-    policy = <<EOF
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM policy for managing aws lambda role"
+  policy      = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -50,15 +50,15 @@ EOF
 
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-    role = aws_iam_role.lambda_role.name
-    policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
 #Genrate an archive from content a file or directory of files
 
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_dir = "${path.module}/python/"
+  source_dir  = "${path.module}/python/"
   output_path = "${path.module}/python/hello-python.zip"
 }
 
@@ -67,23 +67,23 @@ data "archive_file" "zip_the_python_code" {
 #In terraform ${path.module} is the currrent working directory
 
 resource "aws_lambda_function" "terraform_lambda_func" {
-    filename = "${path.module}/python/hello-python.zip"
-    function_name = "Test-lambda-function"
-    role = aws_iam_role.lambda_role.arn
-    handler = "hello-python.lambda_handler"
-    runtime = "python3.8"
-    depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "Test-lambda-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "hello-python.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
 
 output "teraform_aws_role_output" {
- value = aws_iam_role.lambda_role.name
+  value = aws_iam_role.lambda_role.name
 }
 
 output "teraform_aws_role_arn_output" {
- value = aws_iam_role.lambda_role.arn
+  value = aws_iam_role.lambda_role.arn
 }
 
 output "teraform_logging_arn_output" {
- value = aws_iam_policy.iam_policy_for_lambda.arn
+  value = aws_iam_policy.iam_policy_for_lambda.arn
 }
